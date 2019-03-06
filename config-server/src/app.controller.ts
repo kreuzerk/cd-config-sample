b81d35c125048ef54b7f54ed62ff19228c6b6559
kreuzerk <kevin.kreuzer90@icloud.com>
2019-03-06 07:57:43 +0100

    feat(configuration): deliver sample configuration

--- config-server/src/app.controller.ts
+++ config-server/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get} from '@nestjs/common';
+import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Get('/configuration')
+    public getConfiguration(): any {
+        return {
+            backendUrl: process.env.backendUrl,
+            stage: process.env.stage
+        };
+    }
 }
